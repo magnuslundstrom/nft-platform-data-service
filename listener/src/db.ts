36569083a1b065ddeaae07ed58d2ba93a48235a7
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2022-01-10 00:39:22 +0100

    update deps

--- listener/src/db.ts
+++ listener/src/db.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql';
 
 const connection = mysql.createConnection({
-    host: 'db',
+    host: '0.0.0.0',
     user: 'root',
     // replace with process.env values
     password: 'temp',
